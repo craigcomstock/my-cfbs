0c12b47a03c0783bac9653208cce1511b3337643
Craig Comstock <craig.comstock@northern.tech>
2024-08-27 16:25:42 -0500

    libre

---
+++ scripts/cfe.ps1
@@ -0,0 +1,2 @@
+& 'C:\Program Files\Cfengine\bin\cf-agent.exe' -KIf update.cf
+& 'C:\Program Files\Cfengine\bin\cf-agent.exe' -KI
---
+++ scripts/debugpromises.ps1
@@ -0,0 +1,1 @@
+& 'C:\Program Files\Cfengine\bin\cf-agent.exe' -Kd | Set-Content -Path /cf-agent-debug.log
