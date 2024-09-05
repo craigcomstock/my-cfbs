92180d8fda408250e3281a262a032e292371241b
Craig Comstock <craig.comstock@northern.tech>
2024-09-04 19:58:20 -0500

    libre

--- package-module-winget/install-winget-powershell-module.ps1
+++
@@ -1,6 +0,0 @@
-# https://github.com/microsoft/winget-cli
-# WinGet.Client needs NuGet
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name Microsoft.WinGet.Client
-Import-Module -Name Microsoft.WinGet.Client
